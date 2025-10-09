e6bb293c42dcdf4fb9a6e60ea3e0fb30e94fff5a
Shouq bin daws <127682553+ShouqAbdulrahim@users.noreply.github.com>
2025-10-09 14:09:56 +0300

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -24,4 +24,5 @@ resource "azurerm_subnet" "snet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [each.value.cidr]
+  network_security_group_id = azurerm_network_security_group.nsg_frontend.id
 }
