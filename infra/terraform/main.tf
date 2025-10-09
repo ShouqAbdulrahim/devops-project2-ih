ecb7a57f03b98813eb9f0e9e9dc974264ebbbb85
Shouq bin daws <127682553+ShouqAbdulrahim@users.noreply.github.com>
2025-10-09 11:40:56 +0300

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,10 +4,10 @@ locals {
 }
 
 # Resource Group
- resource "azurerm_resource_group" "rg" {
-  name     = local.rg_name
-  location = var.location
-}
+# resource "azurerm_resource_group" "rg" {
+#  name     = local.rg_name
+#  location = var.location
+#}
 
 
 # Virtual Network
